f5eebb3ef74efcf3ae779119728e111334837779
Abu018 <iam.abu.mohammad@gmail.com>
2024-03-31 10:00:44 +0530

    first commit

---
+++ 01-starting-project/src/components/DeleteConfirmation.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+const TIMER = 3000;
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [reamainingTime, setReaminingTime] = useState(TIMER);
+  useEffect(() => {
+    setInterval(() => {
+      setReaminingTime((prev) => prev - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  useEffect(() => {
+    console.log("TIMER SET");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+  return (
+    <div id="delete-confirmation">
+      <h2>Are you sure?</h2>
+      <p>Do you really want to remove this place?</p>
+      <div id="confirmation-actions">
+        <button onClick={onCancel} className="button-text">
+          No
+        </button>
+        <button onClick={onConfirm} className="button">
+          Yes
+        </button>
+      </div>
+      <progress value={reamainingTime} max={TIMER} />
+    </div>
+  );
+}
